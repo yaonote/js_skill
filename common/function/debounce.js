487e890661e597d7a85515686239ecd4f3b07aac
Mr.yao <lily@dehuinet.com>
2019-09-16 18:26:18 +0800

    debounce & throttle

--- common/function/debounce.js
+++ common/function/debounce.js
@@ -3,7 +3,25 @@
  * 防抖
  * @param {*} fn 
  * @param {*} delay 
+ * 
+ * thinking: 
+ *   事件触发N秒后执行回调 N秒内再次触发 则重新计时 
+ *   一段时间内 执行最后一次
+ * 
  */
-const debounce = (fn,delay) => {
 
+const debounce = (fn,delay) => {
+    let timer = null;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            fn.apply(null,args)
+        },delay)
+    }
 }
+const fn = () => {
+    console.log('test')
+}
+const delay = debounce(fn,1000)
+delay();
+delay();
---
+++ common/function/throttle.js
@@ -0,0 +1,30 @@
+/**
+ * 节流 
+ * thinking:  
+ *  多次触发只有一次生效
+ *  保证在间隔时间内最多只触发一次 （像子弹发射）
+ * 
+ *  防抖会执行最后一次
+ *  节流只会执行时间间隔内的第一次 
+ *  之后再找个delay内就不会再执行了
+ *  直到下一个delay 内的第一次触发的事件
+ */
+const throttle = (fn, delay = 500) => {
+    let flag = true;
+    return (...args) => {
+        if(!flag) return ;
+        flag = false;
+        setTimeout(_ => {
+            fn.apply(null,args)
+            flag = true;
+        },delay)
+    } 
+}
+const foo = () => console.log('run foo');
+const throttleDelay = throttle(foo);
+
+
+// setInterval(() => {
+//     throttleDelay()
+// }, 400)
+
