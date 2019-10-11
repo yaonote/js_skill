c2a77b16a917fa5ba7300560bbea54994bf729c2
329181625@qq.com <329181625@qq.com>
2019-10-11 11:32:59 +0800

    添加排序算法

---
+++ 算法/排序/insertionSort.js
@@ -0,0 +1,35 @@
+/**
+ * 插入排序
+ */
+
+ /**
+  * 
+  * @param {*} arr 
+  * @param {*} length 
+  */
+ function insert(arr, length) {
+     let key = arr[length];
+     let i = length;
+     /**
+      * 只要数组的倒数第二个值大于最后一个值
+      * 两者交换位置
+      */
+     while(arr[i - 1] > key){
+         arr[i] = arr[i - 1];
+         i--;
+         if(i == 0) {
+             break;
+         }
+     }
+     arr[i] = key;
+ }
+
+ function insertionSort(arr) {
+    let _length = arr.length;
+    for (let i = 1; i < _length; i++) {
+        insert(arr, i);
+    }
+    return arr;
+ }
+
+ console.log(insertionSort([2,6,3,5,0,4]))
