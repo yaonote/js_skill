ddead40081fa507250c4c9639b718d6196bc85e3
Mr.yao <lily@dehuinet.com>
2019-11-06 12:34:05 +0800

    curry

--- common/function/curry.js
+++ common/function/curry.js
@@ -2,11 +2,13 @@
  * 简易版本
  */
 
- function curry(func,args) {
-    var arity = func.length;
-    var args = args || [];
 
-    return function() {
-        
-    }
- }
+const curry = (fn,...args1) => (...args2) => (
+   arg => arg.length === fn.length ? fn(...arg) : curry(fn,...arg)
+)([...args1,...args2])
+
+const foo = (a,b,c) => a*b*c;
+
+const res = curry(foo)(2,3,4);
+
+console.log('yao-coding res =>',res)
