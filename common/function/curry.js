dfd9d8d6980441e0fe1fe561a138fb3a77f47709
Mr.yao <lily@dehuinet.com>
2019-11-06 13:47:52 +0800

    curry

--- common/function/curry.js
+++ common/function/curry.js
@@ -3,12 +3,24 @@
  */
 
 
-const curry = (fn,...args1) => (...args2) => (
-   arg => arg.length === fn.length ? fn(...arg) : curry(fn,...arg)
-)([...args1,...args2])
+// const curry = (fn,...args1) => (...args2) => (
+//    arg => arg.length === fn.length ? fn(...arg) : curry(fn,...arg)
+// )([...args1,...args2])
+
+// const foo = (a,b,c) => a*b*c;
+// const res = curry(foo)(2,3,4);
+// console.log('yao-coding res =>',res)
+
+
+const curry = (fn,...args1) => (...args2) => {
+   return (function(arg) {
+      return arg.length === fn.length 
+         ? fn(...arg)
+         : curry(fn,...arg)
+   })([...args1,...args2])
+}
 
 const foo = (a,b,c) => a*b*c;
-
 const res = curry(foo)(2,3,4);
-
-console.log('yao-coding res =>',res)
+const res1 = curry(foo)(2)(3,4);
+console.log('yao-coding res1 =>',res1)
