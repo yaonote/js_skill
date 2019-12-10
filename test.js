4379293bdd9cbcbac718ad747deb313a20930ba4
Mr.yao <lily@dehuinet.com>
2019-12-11 00:14:40 +0800

    all

--- test.js
+++ test.js
@@ -1,30 +1,45 @@
 
 
-class UploadTool{
-    constructor() {
-        this.callback = null;
-        this.data = null;
+function getDateDiff(dateTimeStamp){
+    var minute = 1000 * 60;
+    var hour = minute * 60;
+    var day = hour * 24;
+    var halfamonth = day * 15;
+    var month = day * 30;
+    var year = month * 12;
+    var now = new Date().getTime();
+    var diffValue = now - dateTimeStamp;
+    var result;
+    if(diffValue < 0){
+        //若日期不符则弹出窗口告之
+        //alert("结束日期不能小于开始日期！");
     }
+    var yearC = diffValue/year;
+    var monthC =diffValue/month;
+    var weekC =diffValue/(7*day);
+    var dayC =diffValue/day;
+    var hourC =diffValue/hour;
+    var minC =diffValue/minute;
+    if(yearC>=1){
+        result=yearC+ "年前";
 
-    uploadStart(data,callback) {
-        this.data = data;
-        this.callback = callback;
-
-        setTimeout(() => {
-            this.uploadSuccess();
-        },500)
     }
-
-    uploadSuccess() {
-        this.callback(this.data)
+    else if(dayC>=2){
+        result=parseInt(dayC) +"天前";
     }
+    else if(dayC >= 1 && dayC <2) {
+        result="昨天";
+    }
+    else if(hourC>=1){
+        result=parseInt(hourC) +"小时前";
+    }
+    else if(minC>=1){
+        result=parseInt(minC) +"分钟前";
+    }else{
+        result="刚刚";
+    }
+    return result;
 }
 
-const uploadTool = new UploadTool();
 
-uploadTool.uploadStart({fileId: 1},function(data) {
-    console.log(data)
-})
-uploadTool.uploadStart({fileId: 2},function(data) {
-    console.log(data)
-})
+console.log(` 非本年 => ${getDateDiff(1575198149000)}`)
